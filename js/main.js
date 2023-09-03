dd6ee9c6dabc411496cbc96e29cf3069cb4bb739
Alex <a.kuznetsov23@gmail.com>
2023-09-03 11:54:16 +0300

    добавил условие смены день\ночь

--- js/main.js
+++ js/main.js
@@ -10,6 +10,18 @@ const pizzaAddBtn = document.querySelector(".pizzaAddBtn");
 const pizzaRemoveBtn = document.querySelector(".pizzaRemoveBtn");
 const addDiscountbtn = document.querySelector("#addDiscountBtn");
 const discount = document.querySelector(".discount");
+const sunIcon = document.querySelector(".daytime__sun");
+const moonIcon = document.querySelector(".daytime__moon");
+const container = document.querySelector(".container");
+const inscriptions = document.querySelectorAll(".left__fields span");
+const form = document.querySelector(".orderDetails");
+const reservation = document.querySelector(".formHeader span");
+const reservationSubtitle = document.querySelector(".formSubtitle");
+const reservationBorder = document.querySelector(".dataCustomer");
+const search = document.querySelector(".search");
+const currentTime = new Date().toLocaleTimeString();
+const nightTime1 = new Date("Sun Sep 03 2023 19:00:00").toLocaleTimeString();
+const nightTime2 = new Date("Sun Sep 03 2023 06:00:00").toLocaleTimeString();
 
 let cuponNumber = "";
 let totalBill = 0;
@@ -21,7 +33,28 @@ let totalBill = 0;
 
 // cuponCloseBtn.classList.add("cursor");
 
-cuponModal.addEventListener("click", function (e) {
+// function createCuponNumber(e) {
+//   console.log(e);
+//   if (e.target == cuponBody) {
+//     return;
+//   }
+//   cupon.classList.toggle("hide");
+//   e.preventDefault();
+//   e.stopPropagation();
+//   document.body.classList.toggle("overflowYhidden");
+// }
+
+// Array.from(menuItemAddBtn);
+cuponModal.addEventListener("click", createCupon);
+pizzaAddBtn.addEventListener("click", plusPizza);
+pizzaAddBtn.addEventListener("click", plusCost);
+pizzaRemoveBtn.addEventListener("click", minusPizza);
+pizzaRemoveBtn.addEventListener("click", minusCost);
+addDiscountbtn.addEventListener("click", addDiscount);
+moonIcon.addEventListener("click", toggleTimeMode);
+sunIcon.addEventListener("click", toggleTimeMode);
+
+function createCupon(e) {
   for (i = 0; i < 8; i++) {
     cuponNumber += Math.round(Math.random() * 10);
   }
@@ -41,26 +74,7 @@ cuponModal.addEventListener("click", function (e) {
   });
   e.preventDefault();
   e.stopPropagation();
-});
-
-// function createCuponNumber(e) {
-//   console.log(e);
-//   if (e.target == cuponBody) {
-//     return;
-//   }
-//   cupon.classList.toggle("hide");
-//   e.preventDefault();
-//   e.stopPropagation();
-//   document.body.classList.toggle("overflowYhidden");
-// }
-
-// Array.from(menuItemAddBtn);
-
-pizzaAddBtn.addEventListener("click", plusPizza);
-pizzaAddBtn.addEventListener("click", plusCost);
-pizzaRemoveBtn.addEventListener("click", minusPizza);
-pizzaRemoveBtn.addEventListener("click", minusCost);
-addDiscountbtn.addEventListener("click", addDiscount);
+}
 
 function plusPizza() {
   let currentValue = parseInt(amountPizza.innerHTML);
@@ -94,4 +108,36 @@ function addDiscount(e) {
   }
   e.preventDefault();
   discount.value = "";
+}
+
+function toggleTimeMode() {
+  moonIcon.classList.toggle("hide");
+  sunIcon.classList.toggle("hide");
+  container.classList.toggle("bgBlack");
+  container.classList.toggle("bgWhite");
+
+  if (container.classList.contains("bgWhite")) {
+    inscriptions.forEach((el) => {
+      el.style.color = "black";
+    });
+    form.style.background = "#434343";
+    reservation.style.color = "#434343";
+    reservationSubtitle.style.color = "black";
+    reservationBorder.style.border = "1px solid black";
+    search.style.background = "white";
+    search.style.color = "black";
+  } else {
+    inscriptions.forEach((el) => {
+      el.style.color = "white";
+    });
+    form.style.background = "rgb(255, 255, 255, 0.2)";
+    form.style.color = "#dddddd";
+    reservationSubtitle.style.color = "white";
+    reservationBorder.style.border = "1px solid rgb(255, 255, 255, 0.5)";
+    search.style.background = "#151515";
+    search.style.color = "white";
+  }
+}
+if (currentTime < nightTime1 && currentTime > nightTime2) {
+  toggleTimeMode();
 }
